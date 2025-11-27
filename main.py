af6c56659ccfa6545abfe3961ad4b9bdf1c1443d
ayaaburabia <ayaab2@ac.sce.ac.il>
2025-11-27 16:12:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,3 +22,17 @@ def add_book(book: dict):
     global counter
     new_book = {
         "id": counter,
+"title": book.get("title"),
+        "author": book.get("author")
+    }
+    books.append(new_book)
+    counter += 1
+    return new_book
+
+@app.delete("/books/{book_id}")
+def delete_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id:
+            books.remove(book)
+            return {"message": "Book deleted"}
+    raise HTTPException(status_code=404, detail="Book not found")
